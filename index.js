900c6beeb23e8adf6dd8611d02c83931de66b67a
takenokoya <takuya1002@gmail.com>
2020-05-31 17:36:31 +0900

    about props

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const listTitle = {
+const ListTitle = {
     template: `
         <h2>user list</h2>
     `
 }
 
+const UserDetail = {
+    props: {
+        user: {
+            type: Object
+        }
+    },
+    template: `
+        <div>
+            <h2>selected user</h2>
+            {{ user.name }}
+        </div>
+    `
+}
+
 // componentはVueインスタンスの作成前に定義しておく
-Vue.component('user-list', {
+const UserList =  {
     components: {
-        'list-title': listTitle
+        'list-title': ListTitle,
+        'user-detail': UserDetail
     },
     // componentの値は関数の戻り値になるようにする
     data() {
@@ -18,22 +33,27 @@ Vue.component('user-list', {
                 { id: 3, name: 'ユーザー3' },
                 { id: 4, name: 'ユーザー4' },
                 { id: 5, name: 'ユーザー5' }
-            ]
+            ],
+            selected_user: {}
         }
     },
     template: `
         <div>
             <list-title></list-title>
             <ul>
-                <li v-for="user in users" :key="user.id">
+                <li v-for="user in users" :key="user.id" @click='selected_user = user'>
                     {{ user.name }}
                 </li>
             </ul>
+            <user-detail :user='selected_user'></user-detail>
         </div>
 
     `
-})
+}
 
 const vm = new Vue({
     el: '#app',
+    components: {
+        'user-list': UserList
+    }
 })
