b714b9acfc74bed203fd1d9a8f591c95b6946dc5
takenokoya <takuya1002@gmail.com>
2020-05-31 16:52:48 +0900

    abount life cycle hook

--- index.js
+++ index.js
@@ -13,9 +13,42 @@ const vm = new Vue({
     `,
     data() {
         return {
-            message: 'hello'
+            message: 'hello',
+            interval_id: null
         }
+    },
+    beforeCreate() {
+        console.log('before init Vue instance')
+    },
+    created() {
+        console.log('after init Vue instance')
+        this.message = 'incetance initialized'
+
+        let seconds = 1
+        this.interval_id = setInterval(() => {
+            console.log(`${seconds++}second`)
+        }, 1000) 
+    },
+    beforeMount() {
+        console.log('before mount')
+    },
+    mounted() {
+        console.log('after mount')
+    },
+    beforeUpdate() {
+        console.log('before DOM update')
+    },
+    updated() {
+        console.log('after DOM update')
+    },
+    beforeDestroy() {
+        console.log('before destroy Vue instance')
+        clearInterval(this.interval_id)
+    },
+    destroyed() {
+        console.log('after destroy Vue instance')
     }
+
 })
 
 //$mount
